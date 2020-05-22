8b67d8ea5869a537aa60da58c7b9e43f1b55713f
Martin Hansen <code@martin-hansen.de>
2020-05-22 14:45:13 +0200

    Remove unused PlayerState

--- logic/src/main/scala/io/github/mahh/doko/logic/player/PlayerState.scala
+++
@@ -1,9 +0,0 @@
-package io.github.mahh.doko.logic.player
-
-import io.github.mahh.doko.shared.deck.Card
-
-case class PlayerState(
-  initialCards: List[Card],
-  currentCards: List[Card],
-  wonTricks: List[Card]
-)
