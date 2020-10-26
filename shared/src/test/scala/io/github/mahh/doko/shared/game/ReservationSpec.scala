8b4a05796b8c80717f0d65d182054c6f54a60f5c
Martin Hansen <code@martin-hansen.de>
2020-10-26 07:10:29 +0100

    Add another test

---
+++ shared/src/test/scala/io/github/mahh/doko/shared/game/ReservationSpec.scala
@@ -0,0 +1,13 @@
+package io.github.mahh.doko.shared.game
+
+import minitest.SimpleTestSuite
+
+object ReservationSpec extends SimpleTestSuite {
+
+  // this is rather obvious, but somehow null-values appear(ed) to sneak into other tests
+  test("Solo.solo of Solo.all must not be null") {
+    val all = Reservation.Solo.All
+    assert(all.forall(_.solo != null), s"No null in $all")
+  }
+
+}
