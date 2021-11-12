3c81551a7d65e8b26687676b7f410b174e37b7d9
Martin Hansen <code@martin-hansen.de>
2021-11-12 22:03:04 +0100

    Bump dependency versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,13 +2,13 @@ object Versions {
 
   val scalaVersion = "2.13.4"
 
-  val akkaVersion = "2.6.10"
-  val akkaHttpVersion = "10.2.2"
+  val akkaVersion = "2.6.17"
+  val akkaHttpVersion = "10.2.7"
   val logBackVersion = "1.2.3"
-  val circeVersion = "0.13.0"
+  val circeVersion = "0.14.1"
   val scalaJsDomVersion = "1.1.0"
-  val shapelessVersion = "2.3.3"
-  val miniTestVersion = "2.9.1"
-  val scalaCheckVersion = "1.15.1"
+  val shapelessVersion = "2.3.7"
+  val miniTestVersion = "2.9.6"
+  val scalaCheckVersion = "1.15.4"
 
 }
