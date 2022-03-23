3f13ff8957698b72ec549d09c945e041890c1d20
Martin Hansen <code@martin-hansen.de>
2022-03-23 15:24:16 +0100

    Bump akka and logback
    Bump versions of akka, akka-http and logback.

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,9 +2,9 @@ object Versions {
 
   val scalaVersion = "2.13.4"
 
-  val akkaVersion = "2.6.17"
-  val akkaHttpVersion = "10.2.7"
-  val logBackVersion = "1.2.3"
+  val akkaVersion = "2.6.19"
+  val akkaHttpVersion = "10.2.9"
+  val logBackVersion = "1.2.11"
   val circeVersion = "0.14.1"
   val scalaJsDomVersion = "2.0.0"
   val shapelessVersion = "2.3.7"
