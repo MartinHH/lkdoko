9a189df28177a56aac4db2450c7cbbc61a16dbc5
Martin Hansen <code@martin-hansen.de>
2022-07-26 11:45:00 +0200

    Bumps scalajs & scalajs-dom
    Also: remove unnecessary call to UUID.randomUUID() to fix linking
    error.

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val akkaHttpVersion = "10.2.9"
   val logBackVersion = "1.2.11"
   val circeVersion = "0.14.2"
-  val scalaJsDomVersion = "2.0.0"
+  val scalaJsDomVersion = "2.1.0"
   val munitVersion = "0.7.29"
 
 }
