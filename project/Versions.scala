9f214c089968f481c2d337f110ddfa75383d726e
Martin Hansen <code@martin-hansen.de>
2022-09-18 08:45:41 +0200

    General versions bump

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,12 +1,12 @@
 object Versions {
 
-  val scalaVersion = "3.1.3"
+  val scalaVersion = "3.2.0"
 
-  val akkaVersion = "2.6.19"
-  val akkaHttpVersion = "10.2.9"
+  val akkaVersion = "2.6.20"
+  val akkaHttpVersion = "10.2.10"
   val logBackVersion = "1.2.11"
-  val circeVersion = "0.14.2"
-  val scalaJsDomVersion = "2.1.0"
+  val circeVersion = "0.14.3"
+  val scalaJsDomVersion = "2.3.0"
   val munitVersion = "0.7.29"
 
 }
