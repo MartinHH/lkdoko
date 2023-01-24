916121be3f8c313572976d38de1d332c934dc004
Martin Hansen <code@martin-hansen.de>
2023-01-24 11:31:48 +0100

    Use laminar for name input
    This is just a first step towards using laminar for the client.

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,6 +7,7 @@ object Versions {
   val logBackVersion = "1.2.11"
   val circeVersion = "0.14.3"
   val scalaJsDomVersion = "2.3.0"
+  val laminarVersion = "0.14.5"
   val munitVersion = "0.7.29"
   val http4sVersion = "1.0.0-M38"
 }
