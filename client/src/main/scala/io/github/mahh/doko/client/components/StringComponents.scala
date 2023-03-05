b5d74ad958354bc0945c5cbcdbd7c468c36c07d2
Martin Hansen <code@martin-hansen.de>
2023-03-05 11:21:52 +0100

    Client: tweak name input
    - only disable, never hide it
    - fix its logic (= re-allow) for joining state
    - start using laminext core

--- client/src/main/scala/io/github/mahh/doko/client/components/StringComponents.scala
+++ client/src/main/scala/io/github/mahh/doko/client/components/StringComponents.scala
@@ -2,23 +2,23 @@ package io.github.mahh.doko.client.components
 
 import com.raquo.laminar.api.L.*
 import io.github.mahh.doko.shared.msg.MessageToServer.SetUserName
+import io.laminext.syntax.core.*
 
 object StringComponents {
 
   def nameInput(
-    disabledSignal: SignalSource[Boolean],
+    isAllowed: Signal[Boolean],
     commandSink: Observer[SetUserName]
   ): Div =
     div(
       label(
-        "Your name: ",
-        hidden <-- disabledSignal
+        "Your name: "
       ),
       input(
         onMountFocus,
         placeholder := "Enter your name here",
-        hidden <-- disabledSignal,
-        onInput.mapToValue.map(SetUserName.apply) --> commandSink
+        onInput.mapToValue.map(SetUserName.apply) --> commandSink,
+        disabled <-- isAllowed.not
       ),
       p()
     )
