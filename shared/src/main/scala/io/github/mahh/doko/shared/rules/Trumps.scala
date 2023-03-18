2f71d430222f4c4d7935b6b27a264c4ed554b8a1
Martin Hansen <code@martin-hansen.de>
2023-03-18 14:23:57 +0100

    Revert "Remove workaround for initialization bug"
    Turns out that the bug still exists.
    
    This reverts commit 19dcfefb10282bdc80694ebfdc262d5fcf5abcac.

--- shared/src/main/scala/io/github/mahh/doko/shared/rules/Trumps.scala
+++ shared/src/main/scala/io/github/mahh/doko/shared/rules/Trumps.scala
@@ -67,7 +67,10 @@ object Trumps {
 
     case object JacksSolo extends CourtSolo(J)
 
-    val All: List[Solo] =
+    // TODO: this should be a val, but there seem to be some strange initialization order problems
+    //  due to which sometimes members of this are null if this is a val - these should be analyzed
+    //  and fixed differently
+    def All: List[Solo] =
       List(QueensSolo, JacksSolo, ClubsSolo, SpadesSolo, HeartsSolo, DiamondsSolo)
 
   }
