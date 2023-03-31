0558c4d4e304eaffcee7344be14da6e15600d842
Martin Hansen <code@martin-hansen.de>
2023-03-31 23:37:33 +0200

    Test: more tests for DeriveArbitrary

--- shared/src/test/scala/io/github/mahh/doko/shared/testutils/DeriveArbitrarySpec.scala
+++ shared/src/test/scala/io/github/mahh/doko/shared/testutils/DeriveArbitrarySpec.scala
@@ -28,6 +28,15 @@ class DeriveArbitrarySpec extends FunSuite {
   test("Generates same values as non-derived Gen (for simple ADT)") {
     equalValues(SimpleADT.expectedGen)
   }
+
+  test("Generates same values as non-derived Gen (for simple Enum)") {
+    equalValues(ABC.expectedGen)
+  }
+
+  test("Generates same values as non-derived Gen (for more complex example)") {
+    equalValues(ComplexADTWithNestedMembers.expectedGen)
+  }
+
 }
 
 object DeriveArbitrarySpec {
@@ -51,4 +60,70 @@ object DeriveArbitrarySpec {
       } yield SimpleCaseClass(x, y, s)
   }
 
+  enum ABC(asChar: Char) {
+    case A extends ABC('A')
+    case B extends ABC('B')
+    case C extends ABC('C')
+  }
+
+  object ABC {
+    val expectedGen: Gen[ABC] =
+      Gen.oneOf(ABC.A, ABC.B, ABC.C)
+  }
+
+  sealed trait ComplexADTWithNestedMembers
+
+  case object AnotherCaseObject extends ComplexADTWithNestedMembers
+
+  sealed abstract class AbstractSubClass[N <: SimpleADT](abc: ABC)
+    extends ComplexADTWithNestedMembers {
+    def nestedSimple: N
+  }
+
+  object AbstractSubClass {
+    case class SubclassA(a: Int, b: String, nestedSimple: SimpleCaseClass)
+      extends AbstractSubClass[SimpleCaseClass](ABC.A)
+
+    object SubclassA {
+      val expectedGen: Gen[SubclassA] =
+        for {
+          a <- arbitrary[Int]
+          b <- arbitrary[String]
+          n <- SimpleCaseClass.expectedGen
+        } yield SubclassA(a, b, n)
+    }
+
+    case class SubclassB(nestedSimple: SimpleADT) extends AbstractSubClass[SimpleADT](ABC.B)
+
+    object SubclassB {
+      val expectedGen: Gen[SubclassB] =
+        SimpleADT.expectedGen.map(SubclassB.apply)
+    }
+    case class SubclassC(c: String, d: Double, anotherLetter: ABC)
+      extends AbstractSubClass[SimpleCaseObject.type](ABC.C) {
+      override def nestedSimple: SimpleCaseObject.type = SimpleCaseObject
+    }
+
+    object SubclassC {
+      val expectedGen: Gen[SubclassC] =
+        for {
+          c <- arbitrary[String]
+          d <- arbitrary[Double]
+          l <- ABC.expectedGen
+        } yield SubclassC(c, d, l)
+    }
+  }
+
+  object ComplexADTWithNestedMembers {
+    val expectedGen: Gen[ComplexADTWithNestedMembers] =
+      // TODO: ideally, derivation would result in a equal distribution across all four members
+      Gen.oneOf(
+        Gen.const(AnotherCaseObject),
+        Gen.oneOf(
+          AbstractSubClass.SubclassA.expectedGen,
+          AbstractSubClass.SubclassB.expectedGen,
+          AbstractSubClass.SubclassC.expectedGen
+        )
+      )
+  }
 }
