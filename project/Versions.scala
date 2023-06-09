3e82a324934d62cbc0889aa1922b36b2af1afae0
Martin Hansen <code@martin-hansen.de>
2023-06-09 14:27:42 +0200

    chore: bump some version (including scala 3.3.0)

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
 
-  val scalaVersion = "3.2.2"
+  val scalaVersion = "3.3.0"
 
   val pekkoVersion = "0.0.0+26598-de1a5e3b-SNAPSHOT"
   val pekkoHttpVersion = "0.0.0+4295-b475736f-SNAPSHOT"
@@ -12,5 +12,5 @@ object Versions {
   val laminextVersion = "0.14.4"
   val munitVersion = "0.7.29"
   val scalacheckDerivedVersion = "0.2.0"
-  val http4sVersion = "1.0.0-M38"
+  val http4sVersion = "1.0.0-M39"
 }
