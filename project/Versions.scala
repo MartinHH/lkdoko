07ca966dae4d46792d933b63004f2ef6e2c81142
Martin Hansen <code@martin-hansen.de>
2023-09-06 18:07:47 +0200

    chore: bump circe (to 0.14.6)

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val pekkoHttpVersion = "1.0.0"
   val catsVersion = "2.9.0"
   val logBackVersion = "1.2.11"
-  val circeVersion = "0.14.3"
+  val circeVersion = "0.14.6"
   val scalaJsDomVersion = "2.3.0"
   val laminarVersion = "15.0.1"
   val laminextVersion = "0.15.0"
