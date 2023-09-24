0f7d62c0a8d4c8bb8b99958837c11e7425a27fcd
Martin Hansen <code@martin-hansen.de>
2023-09-24 13:06:36 +0200

    chore: general versions bump

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
 
-  val scalaVersion = "3.3.0"
+  val scalaVersion = "3.3.1"
 
   val pekkoVersion = "1.0.1"
   val pekkoHttpVersion = "1.0.0"
@@ -8,9 +8,9 @@ object Versions {
   val logBackVersion = "1.2.11"
   val circeVersion = "0.14.6"
   val scalaJsDomVersion = "2.3.0"
-  val laminarVersion = "15.0.1"
-  val laminextVersion = "0.15.0"
+  val laminarVersion = "16.0.0"
+  val laminextVersion = "0.16.2"
   val munitVersion = "0.7.29"
-  val scalacheckDerivedVersion = "0.4.0"
+  val scalacheckDerivedVersion = "0.4.2"
   val http4sVersion = "1.0.0-M40"
 }
