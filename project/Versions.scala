d9bed06e2d5de05abeb905870330bb732d0b173b
Martin Hansen <code@martin-hansen.de>
2025-02-27 12:07:18 +0100

    chore: bump scalacheck-derived to 0.7.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -12,7 +12,7 @@ object Versions {
   val laminextVersion = "0.17.1"
   val munitVersion = "1.1.0"
   val munitScalacheckVersion = "1.1.0"
-  val scalacheckDerivedVersion = "0.6.0"
+  val scalacheckDerivedVersion = "0.7.0"
   val scribeVersion = "3.15.2"
   val http4sVersion = "1.0.0-M44"
 }
